863da1efa628228ed7ec1ce9b2c4897063e17921
Alex <troshin@funreality.ru>
2021-08-19 09:16:42 +0300

    make a little cleanup tile-server

--- build_server.sh
+++ build_server.sh
@@ -4,4 +4,5 @@ set -ex
 USERNAME=funreality
 # image name
 IMAGE=osm-ts-server
+#sudo docker build --no-cache --network=host -t $USERNAME/$IMAGE:latest ./server
 sudo docker build --network=host -t $USERNAME/$IMAGE:latest ./server
